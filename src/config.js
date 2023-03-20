65d029d1873b39e102214a58b49f93bf0e383017
Manu <76539155+Manu4559@users.noreply.github.com>
2023-03-21 00:17:16 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,10 @@
+import { config } from 'dotenv'
+
+config()
+
+export const PORT = process.env.PORT || 3000
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'jmrfOrtuella54'
+export const DB_HOST = process.env.BD_HOST || 'localhost'
+export const DB_DATABASE = process.env.DB_DATABASE || 'conpanydb'
+export const DB_PORT = process.env.DB_PORT || 3306
