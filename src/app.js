4549b45bfaf3cf5ce827944bb8ef631984142a01
Manu <76539155+Manu4559@users.noreply.github.com>
2023-03-22 03:39:27 +0100

    instalado cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from 'cors'
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(indexRoutes)
 app.use('/api', employeesRoutes)
 app.use((req, res, next) => {
